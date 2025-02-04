0ccf13337e936f0c3aa495f3e57cab70caa15b2b
WelinCode <welin1924@gmail.comgit config --global user.email welin1924@gmail.com>
2025-02-04 09:58:21 -0400

    Form style added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const api_url = 'http://www.omdbapi.com/?apikey=a3142094&s=disney';
 const api_key = 'a3142094';
 export const App = () => {
   return (
-    <div className='app_container'>
+    <div className='app_container max-w-screen px-[80px] bg-red-400'>
       <FormSearch />
     </div>
   );
--- src/Components/FormSearch.jsx
+++ src/Components/FormSearch.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+
 export const FormSearch = () => {
+  const [title, setTitle] = useState('');
+
+  const handledSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setTitle(e.target.value);
+  };
+
   return (
-    <form>
-      <div>
-        <input type='text' placeholder='Ingresa una pelicula...' />
+    <div className='flex flex-col items-center'>
+      <div className='text-center'>
+        <h2 className='text-4xl font-bold my-6'>Old Movies Finder</h2>
+        <form onSubmit={handledSubmit}>
+          <div className='max-w-[100%] mx-auto  flex '>
+            <input
+              className='w-[70%] h-[35px] px-2 rounded-l-[8px] outline-none border-y-[1px] border-l-[1px]'
+              type='text'
+              placeholder='Ingresa una pelicula...'
+              onChange={onChange}
+            />
+            <button className='w-[30%] h-[35px] bg-black text-white rounded-r-[8px] text-center'>
+              Search
+            </button>
+          </div>
+        </form>
       </div>
-    </form>
+    </div>
   );
 };
